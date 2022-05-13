f0973ab09d09db1b11b3814319877ed2b06cb317
Naveen Kumar <naveenthakur0225@gmail.com>
2022-05-13 22:37:15 +0530

    getEventsWithWorkshops function fixed to be worked in unit tests

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Workshop;
+
 
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+
+    public function workshops() {
+        return $this->hasMany(Workshop::class);
+    }
 }
